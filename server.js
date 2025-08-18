c400ecb2f38abe1dcf34fd1d9d563ffc60bec0ef
6upe <katongobupe444@gmail.com>
2025-08-18 16:16:35 -0700

    Task: Stripe Demo

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const postRoutes = require('./routes/postRoutes');
 const teamRoutes = require("./routes/teamRoutes");
 const authRoutes = require("./routes/authRoutes");
 
+const Stripe = require('stripe');
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -27,6 +29,31 @@ app.use('/api', contactRoutes);
 app.use('/api', blogRoutes);
 app.use('/api', postRoutes);
 app.use("/api", teamRoutes);
+
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+app.post("/create-checkout-session", async (req, res) => {
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      line_items: [
+        {
+          price: "price_1RxTjIFfzj9QdbyubOCnQ573", // ✅ real Price ID from Stripe dashboard
+          quantity: 1,
+        },
+      ],
+      mode: "payment",
+      success_url: "http://localhost:5500/success.html",
+      cancel_url: "http://localhost:5500/cancel.html",
+    });
+
+    res.json({ url: session.url });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: err.message });
+  }
+});
+
 
 
 

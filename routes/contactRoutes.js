c3770c39cf2c4a3321ce9c05f1d67c6738660b0e
6upe <katongobupe444@gmail.com>
2025-08-18 10:28:08 -0700

    Task: Auth Setup (Login & signup)

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const { handleContact, getAllContacts } = require('../controllers/contactController');
+const authMiddleware = require('../middleware/authMiddleware');
+
 router.use(authMiddleware);
 
 router.post('/contact', handleContact);
